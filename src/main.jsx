0b21ff8c763d294fd21953ad7d537c90f5c9a118
shreysomani-2703 <sdsomani27@gmail.com>
2026-07-17 16:28:37 +0530

    Use hash router for Github Pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { WorldProvider } from "./context/WorldContext";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { ThemeProvider, CssBaseline } from "@mui/material";
 
 import App from "./App";
@@ -11,11 +11,11 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <BrowserRouter>
+      <HashRouter>
         <WorldProvider>
         <App />
         </WorldProvider>
-      </BrowserRouter>
+      </HashRouter>
     </ThemeProvider>
   </React.StrictMode>
 );
